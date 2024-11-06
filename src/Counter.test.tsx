38ff73f5d0cdd2c948f9f9c2ea6f111b36160093
detective-sokka <saidutt1997@gmail.com>
2024-11-06 00:48:40 -0500

    Add increment and decrement buttons that modify the counter.

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -1,9 +1,42 @@
 import React from "react";
-import { render } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react-native";
 import Counter from "./Counter";
 
 describe("Counter", () => {
     it("renders correctly", () => {
       render(<Counter/>);
     });
+
+    it("shows an intial state of 0", () => {
+      const { queryByText } = render(<Counter/>);
+
+      const intialState = queryByText("Counter:0");
+      expect(intialState).not.toBeNull();
+    });
+
+    it("increments by 1 each time increment is pressed", () => {
+      const { queryByText, getByTestId } = render(<Counter/>);
+
+      const initialState = queryByText("Counter:0");
+      expect(initialState).not.toBeNull();
+
+      const increment = getByTestId("incrementButton");
+      fireEvent.press(increment);
+
+      const oneState = queryByText("Counter:1");
+      expect(oneState).not.toBeNull();
+    });
+
+    it("decrements by 1 each time decrement is pressed", () => {
+      const { queryByText, getByTestId } = render(<Counter/>);
+
+      const initialState = queryByText("Counter:0");
+      expect(initialState).not.toBeNull();
+
+      const decrement = getByTestId("decrementButton");
+      fireEvent.press(decrement);
+
+      const minumOneState = queryByText("Counter:-1");
+      expect(minumOneState).not.toBeNull();
+    })
 });
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,6 +1,17 @@
-import React, { FC } from "react";
-import { Text } from "react-native";
+import React, { FC, useState } from "react";
+import { Button, Text, View } from "react-native";
 
-const Counter: FC = () => <Text>Hello world!👋</Text>
+const Counter: FC = () => {
+    
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <View>
+            <Text>Counter:{counter}</Text>
+            <Button title="Increment" onPress={() => setCounter(counter+1)} testID="incrementButton" />
+            <Button title="Decrement" onPress={() => setCounter(counter-1)} testID="decrementButton" />
+        </View>    
+    );
+};    
 
 export default Counter;
